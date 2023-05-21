9d9e54cf965613b7026c6a2afe5b8f29fd0cf170
Milanparajuli <milanbhai2058@gmail.com>
2023-05-21 07:38:55 +0545

    fragmentation add

---
+++ app/src/main/java/com/milan/notebook/fragment/example/FragmentDemo.java
@@ -0,0 +1,45 @@
+package com.milan.notebook.fragment.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.milan.notebook.R;
+
+public class FragmentDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_demo);
+
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new Fragment1());
+            }
+        });
+
+        Button btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new Fragment2());
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_container,  fragment);
+        transaction.commit();
+    }
+}
